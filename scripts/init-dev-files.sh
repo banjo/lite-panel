e12db9543b2f7823915f7ade2b68e1a1936e37ce
Anton Ödman <anton.odman@gmail.com>
2024-10-13 11:55:25 +0200

    chore: add init dev files script

---
+++ scripts/init-dev-files.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Create the necessary files for development, to mimic the ubuntu production environment
+
+echo "Creating dev directory..."
+mkdir -p .dev-files
+mkdir -p .dev-files/apps
+mkdir -p .dev-files/server
+
+echo "Done!"
