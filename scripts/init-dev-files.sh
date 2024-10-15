55262c23cd010b6f8523ffe1f047d2927374b702
Anton Ödman <anton.odman@gmail.com>
2024-10-15 22:13:19 +0200

    chore: better dev-files script

--- scripts/init-dev-files.sh
+++ scripts/init-dev-files.sh
@@ -2,9 +2,7 @@
 # Create the necessary files for development, to mimic the ubuntu production environment
 
 echo "Creating dev directory..."
-mkdir -p .dev-files
-mkdir -p .dev-files/apps
-mkdir -p .dev-files/server
+mkdir -p .dev-files/{apps,repo,logs,db}
 
 echo "Creating dev files..."
 touch .dev-files/Caddyfile
