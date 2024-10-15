4c4823200d14a3cbbc59eb1f3bf3ea0515be7d9c
Anton Ödman <anton.odman@gmail.com>
2024-10-15 22:13:25 +0200

    chore: better logger defaults

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,6 +1,7 @@
 import { isBrowser } from "@banjoanton/utils";
 import pino, { TransportTargetOptions } from "pino";
 import { Env } from "./env";
+import { isProduction } from "./runtime";
 // * Pino import needs to be default to work in the browser
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -12,24 +13,31 @@ export const createLogger = (name: string) => {
         return pino({ name, level: clientVariables.VITE_LOG_LEVEL || "info" });
     }
 
-    const targets: TransportTargetOptions[] = [
-        {
+    const env = Env.server();
+    const targets: TransportTargetOptions[] = [];
+
+    if (isProduction) {
+        targets.push({
+            target: "pino/file", // Default Pino file target, which in reality writes to stdout
+            options: { destination: 1 }, // '1' represents stdout
+            level: env.LOG_LEVEL ?? "info",
+        });
+    } else {
+        targets.push({
             target: "pino-pretty",
             options: {
                 colorize: true,
                 ignore: "hostname,pid",
             },
             level: "trace",
-        },
-    ];
+        });
+    }
 
     if (!sharedTransport) {
         sharedTransport = pino.transport({
             targets,
         });
     }
-
-    const env = Env.server();
 
     const base = {
         env: env.NODE_ENV,
