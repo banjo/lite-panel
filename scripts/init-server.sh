d6f4d23a37bc4a3f5ee997d42373b71ff5a356e3
Anton Ödman <anton.odman@gmail.com>
2024-10-15 20:48:12 +0200

    chore: add some fun colors to init script

--- scripts/init-server.sh
+++ scripts/init-server.sh
@@ -4,22 +4,22 @@
 set -e # Exit immediately if a command exits with a non-zero status
 
 if [ $EUID != 0 ]; then
-  echo "Please run as root or use sudo."
+  echo -e "${RED}Please run as root or use sudo.${NC}"
   exit
 fi
 
-echo "Creating the necessary directories for the server to run..."
+echo -e "${YELLOW}Creating the necessary directories for the server to run...${NC}"
 mkdir -p /data/lite-panel/{apps,server,logs}
 chmod +x /data/lite-panel/*
-echo "Directories created successfully."
+echo -e "${GREEN}Directories created successfully.${NC}"
 
-echo "Installing the necessary packages for the server to run..."
+echo -e "${YELLOW}Installing the necessary packages for the server to run...${NC}"
 apt-get update -y >/dev/null
 apt-get install -y curl wget git fail2ban >/dev/null
-echo "Packages installed successfully."
+echo -e "${GREEN}Packages installed successfully.${NC}"
 
 if ! [ -x "$(command -v docker)" ]; then
-  echo "Installing Docker..."
+  echo -e "${YELLOW}Installing Docker...${NC}"
   # Add Docker's official GPG key:
   apt-get update >/dev/null 2>&1
   apt-get install ca-certificates curl -y >/dev/null 2>&1
@@ -36,18 +36,18 @@ if ! [ -x "$(command -v docker)" ]; then
   apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y >/dev/null 2>&1
 
   if ! [ -x "$(command -v docker)" ]; then
-    echo "Docker installation failed. Please install it manually."
+    echo -e "${RED}Docker installation failed. Please install it manually.${NC}"
     exit 1
   fi
   systemctl start docker >/dev/null 2>&1
   systemctl enable docker >/dev/null 2>&1
-  echo "Docker installed successfully."
+  echo -e "${GREEN}Docker installed successfully.${NC}"
 else
-  echo "Docker is installed."
+  echo -e "${GREEN}Docker is installed.${NC}"
 fi
 
 if ! [ -x "$(command -v caddy)" ]; then
-  echo "Installing Caddy..."
+  echo -e "${YELLOW}Installing Caddy...${NC}"
   apt-get install -y debian-keyring debian-archive-keyring apt-transport-https curl >/dev/null 2>&1
   curl -1slf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg >/dev/null 2>&1
   curl -1slf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list >/dev/null 2>&1
@@ -56,20 +56,20 @@ if ! [ -x "$(command -v caddy)" ]; then
   systemctl start caddy >/dev/null 2>&1
   systemctl enable caddy >/dev/null 2>&1
   touch /etc/caddy/Caddyfile
-  echo "Caddy installed successfully."
+  echo -e "${GREEN}Caddy installed successfully.${NC}"
 else
-  echo "Caddy is installed."
+  echo -e "${GREEN}Caddy is installed.${NC}"
 fi
 
 if ! command -v ufw-docker &>/dev/null; then
-  echo "Installing ufw-docker..."
+  echo -e "${YELLOW}Installing ufw-docker...${NC}"
   wget -O /usr/local/bin/ufw-docker https://github.com/chaifeng/ufw-docker/raw/master/ufw-docker >/dev/null 2>&1
   chmod +x /usr/local/bin/ufw-docker >/dev/null 2>&1
-  echo "ufw-docker installed successfully."
+  echo -e "${GREEN}ufw-docker installed successfully.${NC}"
 fi
 
 # Setting up ufw rules
-echo "Configuring UFW firewall..."
+echo -e "${YELLOW}Configuring UFW firewall...${NC}"
 ufw default deny incoming >/dev/null 2>&1
 ufw default allow outgoing >/dev/null 2>&1
 ufw allow 22/tcp >/dev/null 2>&1
@@ -79,4 +79,4 @@ ufw --force enable >/dev/null 2>&1
 ufw-docker install >/dev/null 2>&1
 systemctl restart ufw >/dev/null 2>&1
 
-echo "Initialization completed successfully."
+echo -e "${GREEN}Initalization complete.${NC}"
