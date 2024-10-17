1f6bb2963c36f03229c9c21a1b48e5f31f5023eb
Anton Ödman <anton.odman@gmail.com>
2024-10-17 20:55:48 +0200

    chore: add caddy to dev init

--- scripts/init-dev-files.sh
+++ scripts/init-dev-files.sh
@@ -2,11 +2,10 @@
 # Create the necessary files for development, to mimic the ubuntu production environment
 
 echo "Creating dev directory..."
-mkdir -p .dev-files/{apps,repo,logs,db}
+mkdir -p .dev-files/{apps,repo,logs,db,caddy}
 
 echo "Creating dev files..."
 touch .dev-files/Caddyfile
-echo "# lite-panel start" >.dev-files/Caddyfile
-echo "# lite-panel end" >>.dev-files/Caddyfile
+echo -e "# lite-panel start\n# lite-panel end" >.dev-files/Caddyfile
 
 echo "Done!"
