752f4dc4517707e0508ea1422634dbde8c8dc173
Anton Ödman <anton.odman@gmail.com>
2024-10-19 12:22:13 +0200

    chore: update dev init script

--- scripts/init-dev-files.sh
+++ scripts/init-dev-files.sh
@@ -8,4 +8,11 @@ echo "Creating dev files..."
 touch .dev-files/Caddyfile
 echo -e "# lite-panel start\n# lite-panel end" >.dev-files/Caddyfile
 
+echo "{
+  \"domain\": \"localhost\",
+  \"port\": \"8080\",
+  \"serviceName\": \"lite-panel\",
+  \"basicAuth\": \"\"
+}" >.dev-files/config/server-config.json
+
 echo "Done!"
